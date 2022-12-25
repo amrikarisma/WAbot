d8144a01eaf1e0e2504187b079f5749fac7578df
Amri Karisma <amrikarisma@live.com>
2022-12-26 03:56:45 +0700

    update docker and sandbox mode

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const { OpenAIHandler } = require('./src/open_ai');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: ['--no-sandbox'],
+    }
 });
 
 client.on('qr', qr => {
